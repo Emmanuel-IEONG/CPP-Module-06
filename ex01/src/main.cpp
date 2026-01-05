e299d8b2d19c2f23f1de0bbf92a1fe62e8650127
Emmanuel Ieong <eieong@f2r7s18.clusters.42paris.fr>
2026-01-05 11:41:59 +0100

    refactor: ex01 main

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eieong <eieong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 14:20:52 by eieong            #+#    #+#             */
-/*   Updated: 2025/12/15 14:42:55 by eieong           ###   ########.fr       */
+/*   Updated: 2026/01/05 11:40:58 by eieong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,19 @@ int	main()
 	Data	*deserialized = Serializer::deserialize(raw);
 	std::cout << data;
 	std::cout << "Original ptr: " << ptr << std::endl;
+	std::cout << "Raw : " << raw << std::endl;
 	std::cout << "Deserialized ptr: " << deserialized << std::endl;
 	std::cout << "Deserialized Data: " << *deserialized << std::endl;
 	std::cout << "-------------------------- CHANGING VALUE --------------------------" << std::endl;
 	deserialized->some = "CHANGED";
-	std::cout << "Deserialized Data: " << *deserialized;
-	std::cout << data << std::endl;
+	std::cout << "Deserialized " << *deserialized;
+	std::cout << "             " << data << std::endl;
 	std::cout << "-------------------------- TEST PTR NULL --------------------------" << std::endl;
 	Data	*ptr_null = NULL;
 	uintptr_t	raw_null = Serializer::serialize(ptr_null);
 	Data	*deserialized_null = Serializer::deserialize(raw_null);
 	std::cout << "Original ptr: " << ptr_null << std::endl;
+	std::cout << "Raw : " << raw_null << std::endl;
 	std::cout << "Deserialized ptr: " << deserialized_null << std::endl;
 	
 	return (0);
